f17cb62bfa674152334a8a7fc72d5357ce73cd3a
XingYun <1920991993@qq.com>
2026-03-17 18:33:24 +0800

    Debug
     修复了MyBatis 在云端的严格类加载机制（打成 jar 包运行后）找不到您的数据库 Mapper 接口 导致的连锁崩溃。

--- src/main/java/com/example/leafquery/LeafqueryApplication.java
+++ src/main/java/com/example/leafquery/LeafqueryApplication.java
@@ -2,8 +2,10 @@ package com.example.leafquery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.example.leafquery.mapper")
 public class LeafqueryApplication {
 
 	public static void main(String[] args) {
